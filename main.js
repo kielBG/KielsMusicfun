f57a16d49b2966c6dc6296c5827fbbf2a70ae3f8
kiel buchergallimore <kielbucher@gmail.com>
2023-10-24 15:38:12 -0500

    finished?

--- main.js
+++ main.js
@@ -113,11 +113,148 @@ piano.rest('quarter');
 piano.note('quarter', 'A3');
 piano.note('quarter', 'G3');
 
+//mo drums
+drum.rest('half');
+drum.repeatStart();
+drum.rest('eighth');
+drum.note('eighth', 'A4');
+drum.note('eighth', 'A4');
+drum.rest('eighth');
+drum.note('eighth', 'A4');
+drum.rest('quarter');
+drum.note('eighth', 'A4');
+drum.note('eighth', 'A4');
+drum.rest('eighth');
+drum.note('eighth', 'A4');
+drum.rest('eighth');
+drum.note('eighth', 'A4');
+drum.note('eighth', 'A4');
+drum.note('eighth', 'A4');
+drum.note('eighth', 'A4');
+drum.repeat(2);
+drum.rest('whole');
+drum.rest('quarter');
+drum.note('eighth', 'A4');
+drum.rest('eighth');
+drum.note('eighth', 'A4');
+drum.rest('eighth');
+drum.note('eighth', 'A4');
+drum.rest('eighth');
+
+//cruising and moving
+piano.note('quarter','E4');
+piano.note('quarter','D4');
+piano.note('quarter', 'C4');
+piano.note('half', 'D4');
+piano.note('half', 'A3');
+piano.note('quarter','D4');
+piano.note('quarter','E4');
+piano.note('quarter','D4');
+piano.note('quarter', 'C4');
+piano.note('half', 'D4');
+piano.note('quarter', 'A3');
+piano.rest('eighth');
+piano.note('eighth', 'A3');
+piano.note('quarter', 'D4');
+piano.note('quarter','E4');
+piano.note('quarter','D4');
+piano.note('quarter', 'C4');
+piano.note('half', 'D4');
+piano.note('half', 'C4');
+piano.note('quarter', 'D4');
+piano.note('quarter','E4');
+piano.note('quarter','C4');
+piano.note('eighth', 'A3');
+piano.note('eighth','C4');
+piano.note('quarter', 'D4');
+piano.note('eighth', 'E4');
+piano.note('eighth','E4');
+piano.note('eighth', 'D4');
+piano.note('quarter','C4');
+piano.note('quarter','C4');
+piano.rest('eighth');
+piano.rest('half');
+
+//SHAKE IT OFF
+
+drum.repeatStart();
+drum.rest('eighth');
+drum.note('eighth', 'A4');
+drum.note('eighth', 'A4');
+drum.rest('eighth');
+drum.note('eighth', 'A4');
+drum.rest('quarter');
+drum.note('eighth', 'A4');
+drum.note('eighth', 'A4');
+drum.rest('eighth');
+drum.note('eighth', 'A4');
+drum.rest('eighth');
+drum.note('eighth', 'A4');
+drum.note('eighth', 'A4');
+drum.note('eighth', 'A4');
+drum.note('eighth', 'A4');
+drum.repeat(7);
+
+piano.repeatStart();
+piano.note('eighth', 'A3');
+piano.note('eighth','C4');
+piano.note('eighth','D4');
+piano.note('eighth', 'D4');
+piano.note('eighth','D4');
+piano.note('eighth', 'E4');
+piano.note('quarter', 'C4');
+piano.note('quarter','A3');
+piano.note('quarter', 'G3');
+piano.note('quarter','E3');
+piano.note('quarter','C3');
+piano.note('eighth','A3');
+piano.note('eighth', 'C4');
+piano.note('eighth','D4');
+piano.note('eighth', 'D4');
+piano.note('eighth','D4');
+piano.note('eighth', 'E4');
+piano.note('quarter', 'C4');
+piano.note('quarter','A3');
+piano.note('quarter', 'G3');
+piano.note('quarter','E3');
+piano.note('quarter','C3');
+piano.note('eighth','C4');
+piano.note('eighth', 'C4');
+piano.note('eighth','D4');
+piano.note('eighth', 'D4');
+piano.note('eighth','D4');
+piano.note('eighth', 'E4');
+piano.note('quarter', 'C4');
+piano.note('quarter','A3');
+piano.note('quarter', 'G3');
+piano.note('quarter','E3');
+piano.note('quarter','C3');
+piano.rest('eighth');
+piano.note('eighth', 'C4');
+piano.note('eighth','D4');
+piano.note('eighth', 'E4');
+piano.note('quarter','C4');
+piano.rest('eighth');
+piano.note('eighth', 'C4');
+piano.note('eighth','D4');
+piano.note('eighth', 'E4');
+piano.note('quarter','C4');
+piano.rest('half');
+piano.repeat();
 var player = conductor.finish();
 
-const playButton = document.querySelector('#play')
+const playButton = document.querySelector('#play');
+const pauseButton = document.querySelector('#pause');
+const stopButton = document.querySelector('#stop');
 playButton.addEventListener("click", function () {
     player.play();
+});
+pauseButton.addEventListener("click", function() {
+    player.pause();
+});
+stopButton.addEventListener("click", function() {
+    player.stop(true);
 })
 
 })
+
